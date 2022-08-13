e0980294b60c8ad900a1b74a0d08329fa1daabe4
Franklin Volcic <fvolcic@umich.edu>
2022-08-13 19:10:28 -0400

    Updated some things

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { useId, useState } from 'react';
 import './App.css';
 
-import qs from 'querystring';
+import qs, { stringify } from 'querystring';
+import JSEncrypt from 'jsencrypt';
+
 
 var client_id = '231521e53a6d4fe1b1729d4037b2efb2'; // Your client id
 var client_secret = '627eb37c52a4409d861273d9ac6d4768'; // Your secret
-var redirect_uri = 'http://localhost:3000/'; // Your redirect uri
+var redirect_uri = 'fvolcic.github.io/SampleSite';//'http://localhost:3000/'; // Your redirect uri
 
 var stateKey = 'spotify_auth_state';
 
@@ -49,6 +51,9 @@ function App() {
 }
 
 function SpotifyLogin(props){
+  if(hasAuthCode()){
+    return <div> <p style={{color:'green'}}>Successfully authenticated with spotify!</p> </div>
+  }
   return (
     <div>
       <button onClick={onSpotifyLogin}>Login with spotify</button>
@@ -83,12 +88,31 @@ function Input(props) {
 }
 
 function onDownloadClick() {
+  var params = qs.decode(window.location.href.split('?')[1]);
+
+  // error if hasAuthCode is false
+  if(!hasAuthCode()) {
+    return;      
+  }
+  var code = params.code;
+
   download('credentials.txt', generateDownloadDocument(getWifiPASS(), getWifiSSID()));
 }
 
-function generateDownloadDocument(wifiPass, wifiSSID) {
-  var text = 'wifi_pass=' + wifiPass + '\nwifi_ssid=' + wifiSSID;
-  return text;
+function generateDownloadDocument(wifiPass, wifiSSID, code) {
+  var text = 'wifi_pass=' 
+  + wifiPass 
+  + '\nwifi_ssid=' 
+  + wifiSSID 
+  + '\nspotify-code='
+  + code;
+
+  // encrypt text
+  var encrypt = new JSEncrypt();
+  encrypt.setPublicKey('MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCajqEJLa/za89ML59mBpO7eRNDJpqTagTGBxYQYomk+PhFRQAkx+J1EeiqSjwBghHPYRDfJ03SSrjTB7R2nEIiLhpvvsLbP0Qm48zfK5v70Q3C/68kI8SJ/RKNsMnn29ksclA64WNQrkYEJ4UPuJOJw9Exu/1Hpsbb7fKdbUtRHwIDAQAB/rq6/VYI6gCMkdluuUgXVo4olW3Gn/ej2WhJKpLTYv9DZHAW+5YgauCCAEFaDDmFmjL9nLmOEV1/Wock/3myLSaaqg/rTdeQDU/C1Hxtn8oXcfGwIDAQAB');
+
+  var encrypted = encrypt.encrypt(text);
+  return encrypted;
 }
 
 function download(filename, text) {
@@ -102,6 +126,11 @@ function download(filename, text) {
   element.click();
 
   document.body.removeChild(element);
+}
+
+function hasAuthCode() {
+  console.log("Checking for auth code")
+  return qs.decode(window.location.href.split('?')[1]).code !== undefined;
 }
 
 var generateRandomString = function(length) {
